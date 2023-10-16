628d9590d452ed015838eb1c5333302671082cca
Shaik Md N Rasool <rasool.sk@hotmail.com>
2023-10-16 13:33:00 +0000

    complete header addition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,22 @@ import SignIn from './assets/pages/SignIn';
 import SignUp from './assets/pages/SignUp';
 import About from './assets/pages/About';
 import Profile from './assets/pages/Profile';
+import Header from './assets/components/Header'
 
 
-function App() {
+export default function App() {
   return (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/sign-in" element={<SignIn/>} />
-      <Route path="/sign-up" element={<SignUp/>} />
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/about" element={<About/>} />
-  </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+
