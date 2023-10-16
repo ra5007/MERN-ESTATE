03b9bcecf48663caed4aafa0249e4b8bcd8df2ec
Shaik Md N Rasool <rasool.sk@hotmail.com>
2023-10-16 12:20:59 +0000

    adding all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import Home from './assets/pages/Home';
+import SignIn from './assets/pages/SignIn';
+import SignUp from './assets/pages/SignUp';
+import About from './assets/pages/About';
+import Profile from './assets/pages/Profile';
+
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>App</h1>
-    </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/about" element={<About/>} />
+  </Routes>
+  </BrowserRouter>
   )
 }
 
