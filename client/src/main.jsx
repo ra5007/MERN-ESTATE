71557601fe7f9d2468d5701bd6f824689cdcc546
Shaik Md N Rasool <rasool.sk@hotmail.com>
2023-10-31 19:03:06 +0000

    adding missing code and packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import ReactDOM from 'react-dom/client'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading ={null} persistor = {persistor}>
-    <App />
+      <App />
     </PersistGate>
   </Provider>
-)
+);
